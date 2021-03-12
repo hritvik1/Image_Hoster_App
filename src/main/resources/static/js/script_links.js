263de0580a5e7b1587a54fe0fc9dd31aca4838dc
Hritvik Maini <hritvikmaini00@gmail.com>
2021-03-12 09:52:00 +0530

    Added File Size Check Feature

--- src/main/resources/static/js/script_links.js
+++ src/main/resources/static/js/script_links.js
@@ -14,3 +14,14 @@ function disableUploadBtn() {
     document.getElementById("uploadBtn").style.display = "none"
     document.getElementById("logoutBtn").style.marginRight = "71%"
 }
+
+function fileValidate() {
+    const file     = document.getElementById("image"),
+          fileSize = (file.files.item(0).size)/(1024*1024)
+
+    if(fileSize > 10) {
+        alert("File Size Must Be Less Than 10 MB")
+        return false
+    }
+    return true
+}
